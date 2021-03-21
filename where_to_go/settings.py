db122aa906dc71027beeec3a93d5c7738bd9608d
Tenundor <tenundor@gmail.com>
2021-03-21 14:29:30 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -102,12 +102,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from where_to_go import views
@@ -5,4 +7,4 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
